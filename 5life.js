4e4ddf576cbf0f9253e13de8671a09bc819dff6e
Savino Jossi <savino.jossi@gmail.com>
2014-01-21 17:38:49 +0100

    isAlive test and next-gen array generation added

--- 5life.js
+++ 5life.js
@@ -1,6 +1,8 @@
 (function(){
 var ctx,
-    canvas;
+    canvas,
+    current = [],
+    next = [];
 
 var Config = {
     WIDTH: 800,
@@ -9,6 +11,17 @@ var Config = {
 };
     
 window.addEventListener('load', init, false);
+
+window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              window.oRequestAnimationFrame      ||
+              window.msRequestAnimationFrame     ||
+              function(/* function */ callback, /* DOMElement */ element){
+                window.setTimeout(callback, 1000 / 60);
+              };
+})();
 
 function init(){
     canvas = document.getElementById('board');
@@ -23,9 +36,24 @@ function init(){
     canvas.focus();
 
     drawgrid();
+    generateboard();
+
+    // initial seed for testing
+    //
+    current[10][12] = 1;
+    current[50][70] = 1;
+    current[50][71] = 1;
+    current[95][140] = 1;
+    current[410][120] = 1;
+    current[450][300] = 1;
+    current[500][250] = 1;
+    
 
     console.log('game loaded');
+
+    runLoop();
 }
+
 
 function drawgrid(){
     // horizontal
@@ -47,9 +75,93 @@ function drawgrid(){
     console.log('grid drawn');
 }
 
-function makegridarray(){
-    
+function applytonext(current, next, action){
+    for(var i = 0;i < Config.WIDTH;i++){
+        for( var j = 0;j < Config.HEIGHT;j++){
+		action(next, i, j);
+	}
+    }
 }
 
+function generateboard(){
+    for(var i = 0;i < Config.WIDTH;i++){
+    current[i] = [];
+    next[i] = [];
+        for(var j = 0;j < Config.HEIGHT;j++){
+		current[i][j] = 0;
+		next[i][j] = 0;
+	}
+    }
+}
+
+function isalive(board, x, y){
+	var sum = 0;
+
+	if(x>0 && y>0){
+		board[x-1][y-1] == 1 ? sum += 1 : sum +=0;
+	}
+	if(y>0){
+		board[x][y-1] == 1 ? sum += 1 :sum +=0;
+	}
+	if(x<Config.WIDTH && y>0){
+		board[x+1][y-1] == 1 ? sum += 1:sum +=0;
+	}
+	if(x>0){
+		board[x-1][y] == 1 ? sum += 1 :sum +=0 ;
+	}
+	if(x<Config.HEIGHT){
+		board[x+1][y] == 1 ? sum += 1:sum +=0;
+	}
+	if(x>0 && y<Config.HEIGHT){
+		board[x-1][y+1] == 1 ? sum += 1 : sum +=0;
+	}
+	if(y<Config.HEIGHT){
+		board[x][y+1] == 1 ? sum += 1:sum +=0;
+	}
+	if(x<Config.WIDTH && y<Config.HEIGHT){
+		board[x+1][y+1] == 1 ? sum += 1:sum +=0;
+	}
+
+
+	// check neighbors
+	//
+	if(sum == 2 || sum == 3){
+		console.log('alive!');
+		return true;
+	}else{
+		console.log('dead!');
+		return false;
+	}
+}
+
+function nextgen(){
+    applytonext(current, next, function(cell, x, y){
+	    if(isalive(current, x, y)){
+    		cell = 1;
+	    }
+    	});
+}
+
+function drawboard(){
+    for(var i=0;i<Config.WIDTH;i++){
+	    for(var j=0;j<Config.HEIGHT;j++){
+		if(current[i][j] == 1){
+			ctx.beginPath();
+			ctx.rect(i, j, Config.GRIDSIZE, Config.GRIDSIZE);
+			ctx.fillStyle = 'white';
+			ctx.fill();
+		}
+	    }
+	}
+}
+
+function runLoop(){
+    // calculate next generation
+    nextgen();
+    // draw	
+    drawboard();
+
+    //window.requestAnimFrame(runLoop(), ctx.canvas);
+}
 
 })();
